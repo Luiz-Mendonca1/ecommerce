29d03716628e1b086a23a88f9a82918b5bcc0e71
Luiz Mendonça <luizeduardomendonca77@gmail.com>
2025-07-20 21:34:26 -0300

    add cart page

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom'
 
 export function Header() {
   return (
-    <header className='w-full px-1 bg-slate-200'>
+    <header className='w-full px-1 bg-slate-900'>
     
       <nav className='w-full max-w-7xl h-14 flex items-center justify-between mx-auto'>
-        <Link to="/" className='white rounded-md px-2 py-1 hover:bg-slate-300 transition-colors'>Home </Link>
-        <Link to="/cart" className='flex items-center gap-2 text-slate-800 bg white rounded-md px-2 py-1 hover:bg-slate-300 transition-colors'>
+        <Link to="/" className='bg-slate-100 rounded-md px-2 py-1 hover:bg-slate-300 transition-colors'>Home </Link>
+        <Link to="/cart" className='flex items-center gap-2 text-slate-100 bg-white rounded-md px-2 py-1 hover:bg-slate-300 transition-colors'>
           <FiShoppingCart className=''size={24} color='#121212'/>
-          <span className='text-sm bg-gray-700 text-white rounded-full px-2 py-1'>
+          <span className='text-sm bg-gray-700 rounded-full px-2 py-1'>
             2
           </span>
         </Link>
--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,8 +1,30 @@
 export function Cart() {    
   return (
-    <div>
-      <h1>Your Shopping Cart</h1>
-      <p>This is where you can view and manage your cart items.</p>
+    <div className=" h-fit">  
+      <div className="w-full max-w-7xl mx-auto  pb-4">  
+        <h1 className="font-medium text-2xl text-center py-4 text-white">My cart</h1>
+        <section className="flex items-center justify-between border-b-2 border-slate-800">
+          <img 
+            src="https://i.pinimg.com/1200x/f2/57/d9/f257d95d4f443b372de1f923bc85eaca.jpg" 
+            alt="produto logo" 
+            className="w-28"
+          />
+          <strong className="text-white">Price: 120.00</strong>
+          <div className="flex items-center justify-center gap-3">
+            <button className="bg-white px-2 cursor-pointer rounded text-black font-medium flex items-center justify-center">
+              -
+            </button>
+            <strong className="text-white">2</strong>
+            <button className="bg-white px-2 cursor-pointer rounded text-black font-medium flex items-center justify-center">
+              +
+            </button>
+          </div>
+          <strong className="text-white float-right">
+            Subtotal: 120.00
+          </strong>
+        </section>
+        <strong className="text-white block mt-4">Total: 120.00</strong>  {/* ← Adicione block mt-4 para espaçamento controlado */}
+      </div>
     </div>
-  )
+  );
 }
