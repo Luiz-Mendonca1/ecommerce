8ff87313be9699e8b1a8c81cdccdf7b6a0cfbbed
Luiz Mendonça <luizeduardomendonca77@gmail.com>
2025-07-21 08:50:10 -0300

    add consumo api local

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,25 +1,53 @@
 import { BsCartPlus } from "react-icons/bs";
+import { api } from "../../services/api";
+import { useEffect, useState } from "react";
+
+interface ProductProps{
+    id:number,
+    title:string,
+    description: string,
+    price: number,
+    cover: string
+}
 
 export function Home() {
+    const [product, setProduct] = useState<ProductProps[]>([])
+
+    useEffect(()=>{
+        async function getProduct(){
+            const response = await api.get('/products')
+            setProduct(response.data)
+        }
+
+        getProduct()
+    },[])
   return (
     <div className="min-h-screen ">
-        <main className="w-full max-w-7xl px-4 mx-auto "></main>
+        <main className="w-full max-w-7xl px-4 mx-auto ">
         <div className="grid grid-cols-1 gap-6  md:grid-cols-2 lg:grid-cols-5">
-            <section className="w-full">
-                <img src="https://i.pinimg.com/736x/69/e9/1c/69e91c687144aec896a9e0e6241afd9d.jpg" 
-                alt="logo produto" />
-                <p className="font-medium mt-1 mb-2 text-zinc-100">produto poster</p>
+            {product.map((product)=>(
+                <section key={product.id} className="w-full">
+                <img src={product.cover} 
+                alt={product.title} />
+                <p className="font-medium mt-1 mb-2 text-zinc-100">{product.title}</p>
                 <div className="flex items-center gap-3">
                     <strong className="text-zinc-100 text-lg">
-                        R$16.00
+                        {product.price.toLocaleString('pt-br', {
+                            style:'currency',
+                            currency: 'brl'
+                        })}
                     </strong>
                     <button className="bg-white text-white rounded-md px-2 py-1 hover:bg-slate-300 transition-colors">
                         <BsCartPlus size={20} color="#000000" />
                     </button>
                 </div>
             </section>
+            ))}
         </div>
-
+        </main>
     </div>
+    
+   
+    
   )
 }
